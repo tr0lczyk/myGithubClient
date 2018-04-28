ce0f95263963b7580629ec4d8bf98b3ef51b8ad1
tr0lczyk <matt.olczyk@gmail.com>
2018-04-28 19:45:31 +0200

    layout and search layout finnished, adding fragments and fragment adapter

---
+++ app/src/main/java/com/example/android/mygithubclient/GitHubFragmentAdapter.java
@@ -0,0 +1,6 @@
+package com.example.android.mygithubclient;
+
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class GitHubFragmentAdapter extends FragmentPagerAdapter {
+}
--- app/src/main/java/com/example/android/mygithubclient/MainActivity.java
+++ app/src/main/java/com/example/android/mygithubclient/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.mygithubclient;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ViewPager pager = findViewById(R.id.view_pager);
+        GitHubFragmentAdapter adapter = new GitHubFragmentAdapter(this, getSupportFragmentManager());
+
     }
 }
