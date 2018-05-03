bf6cf0472b3c384a80aa5fa1add9ba4d112f4d84
tr0lczyk <matt.olczyk@gmail.com>
2018-05-03 23:35:22 +0200

    changes

--- app/src/androidTest/java/com/example/android/mygithubclient/util/DateUtilsTest.java
+++ app/src/androidTest/java/com/example/android/mygithubclient/util/DateUtilsTest.java
@@ -2,11 +2,11 @@ package com.example.android.mygithubclient.util;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class DateUtilsTest {
 
     @Test
     public void isoDateToNormal() {
+
+
     }
 }
--- app/src/main/java/com/example/android/mygithubclient/util/DateUtils.java
+++ app/src/main/java/com/example/android/mygithubclient/util/DateUtils.java
@@ -1,6 +1,5 @@
 package com.example.android.mygithubclient.util;
 
-import android.annotation.SuppressLint;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -9,14 +8,14 @@ public class DateUtils {
     private DateUtils(){}
 
     public static String isoDateToNormal(String isoDate){
-        @SuppressLint("SimpleDateFormat") SimpleDateFormat sd1 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+        SimpleDateFormat sd1 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
         Date date = new Date();
         try{
             date = sd1.parse(isoDate.replaceAll("Z$", "+0000"));
         } catch(Exception e){
             System.out.println(e.getMessage());
         }
-        @SuppressLint("SimpleDateFormat") SimpleDateFormat sd2 = new SimpleDateFormat("dd-MM-yyyy");
+        SimpleDateFormat sd2 = new SimpleDateFormat("dd-MM-yyyy");
         return sd2.format(date);
     }
 }
