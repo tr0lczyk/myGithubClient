ce4ec9a8e6bfef2216ee4802b45251d6314acc19
tr0lczyk <matt.olczyk@gmail.com>
2018-05-04 23:08:08 +0200

    null handle, few strings transfer #endIsNight

--- app/src/main/java/com/example/android/mygithubclient/DetailsFragment.java
+++ app/src/main/java/com/example/android/mygithubclient/DetailsFragment.java
@@ -8,6 +8,10 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.example.android.mygithubclient.util.DateUtils.isoDateToNormal;
 import static com.example.android.mygithubclient.util.FileUtils.getFileSize;
 
@@ -19,6 +23,7 @@ public class DetailsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.detail_layout, container, false);
+        List<TextView> sections = new ArrayList<>();
 
         Repository currentRepository = getArguments().getParcelable("object");
 
@@ -27,9 +32,11 @@ public class DetailsFragment extends Fragment {
 
         TextView ownerLogin = rootView.findViewById(R.id.ownerLogin);
         ownerLogin.setText(currentRepository.getUserLogin());
+        sections.add(ownerLogin);
 
         TextView repositoryName = rootView.findViewById(R.id.repositoryName);
         repositoryName.setText(currentRepository.getName());
+        sections.add(repositoryName);
 
         TextView starsCount = rootView.findViewById(R.id.starsNumber);
         starsCount.setText(currentRepository.getStarsGiven());
@@ -45,12 +52,14 @@ public class DetailsFragment extends Fragment {
 
         TextView language = rootView.findViewById(R.id.programmingDescription);
         language.setText(currentRepository.getProgrammingLanguage());
+        sections.add(language);
 
         TextView issuesCount = rootView.findViewById(R.id.issuesNumber);
         issuesCount.setText(currentRepository.getIssuesCount());
 
         TextView branchType = rootView.findViewById(R.id.branchType);
         branchType.setText(currentRepository.getBranchesType());
+        sections.add(branchType);
 
         TextView createdDate = rootView.findViewById(R.id.createDate);
         createdDate.setText(isoDateToNormal((currentRepository.getCreatedDate())));
@@ -61,10 +70,17 @@ public class DetailsFragment extends Fragment {
         TextView updatedDate = rootView.findViewById(R.id.updatedDate);
         updatedDate.setText(isoDateToNormal((currentRepository.getUpdateDate())));
 
+        String defaultStatement = getString(R.string.no_info);
+        for (int i = 0; i < sections.size(); i++) {
+            TextView currentTextView = sections.get(i);
+            if(currentTextView.getText().equals(getString(R.string.only_null))){
+                currentTextView.setText(defaultStatement);
+            }
+        }
         return rootView;
     }
 
     private String toRepositoryType(boolean isOpenSource){
-        return isOpenSource ? "Private" : "Public";
+        return isOpenSource ? getString(R.string.privates) : getString(R.string.publics);
     }
 }
